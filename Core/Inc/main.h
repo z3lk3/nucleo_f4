2f55ccc722e53df290b49ad93b344208e09f517e
Ivan <im.ivan.im@gmail.com>
2022-02-04 23:22:52 +0100

    GPIO_Init
    Red&Blue_Led

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -58,6 +58,10 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LEDB1_Pin GPIO_PIN_8
+#define LEDB1_GPIO_Port GPIOB
+#define LEDR1_Pin GPIO_PIN_9
+#define LEDR1_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
